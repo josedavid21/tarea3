553306e8bd7cdcba788b38cb304d574a5f4b98d0
Cristofhersj <71050835+Cristofhersj@users.noreply.github.com>
2020-10-11 21:44:14 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+	name='Paquete_Tarea3',
+	version='0.1',
+	package_dir= {'Tarea3':'../Proyecto'}, ###########
+	packages= ["Tarea3"], #find_packages(),
+	description='El paquete requerido para la solución de la terea número 3 del curso microprocesadores y microcontroladores',
+	# long_description=open('README.txt').read(),
+	scripts= ["MetodoSonido.py","imagen.py"],
+	install_requires=['playsound==1.2.2',
+	     'pillow>=7.0.0',
+	     'tabulate>=0.8.7',
+	     'argparse>=1.4.0'
+	      ],
+	Python_requires= '>=3',
+	    
+	url='https://github.com/josedavid21/tarea3',
+	author='Mendez, Rojas, Solís, Soto ',
+	author_email='cristofhersj@gmail.com, josedavidsz@hotmail.com'
+	) 
