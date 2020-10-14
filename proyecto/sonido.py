54dc6b84f219ee94e6b61adaba6bbd57a167e094
josedavid21 <70824111+josedavid21@users.noreply.github.com>
2020-10-14 12:30:51 -0600

    Update sonido.py

--- proyecto/sonido.py
+++ proyecto/sonido.py
@@ -3,12 +3,6 @@ import argparse
 import time 
 from playsound import playsound
 
-parser = argparse.ArgumentParser()
-parser.add_argument("Dir",help="La dirección donde se encuentra el archivo mp3 que se desea reproducir incluyendo a este último, por ejemplo: /home/cristofhersj/Music/Sound.mp3",type=str)
-parser.add_argument("num",help="El número de veces que desee reproducir el archivo mp3",type=int)
-parser.add_argument("-t", "--time", help= "Es el tiempo de ejecución del método. ", action="store_true")
-args= parser.parse_args()
-
 
 # Se define la función que reproduce el sonido entradas: 
 # Dir: la dirección del mp3 por reproducir, 
@@ -23,6 +17,12 @@ def Presentador_de_sonido(Dir, NUM):
 # Se define la función principal que contiene el scripting y ejecuta 
 # las funciones dependiendo de si se pide el tiemmpo de ejecución o no 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Dir",help="La dirección donde se encuentra el archivo mp3 que se desea reproducir incluyendo a este último, por ejemplo: /home/cristofhersj/Music/Sound.mp3",type=str)
+    parser.add_argument("num",help="El número de veces que desee reproducir el archivo mp3",type=int)
+    parser.add_argument("-t", "--time", help= "Es el tiempo de ejecución del método. ", action="store_true")
+    args= parser.parse_args()
+
     if args.time:
         tiempoini = time.time_ns() 
         Presentador_de_sonido(args.Dir,args.num)
